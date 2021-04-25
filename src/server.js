c178c54773b929b4fb8767598d00439fbaaa8cde
MartinTiangco <50017873+MartinTiangco@users.noreply.github.com>
2021-04-26 11:37:53 +1200

    CC-012/heroku Enable correct deploys to Heroku (#35)
    Heroku app correctly shows the React application, instead of the backend API messages. The deploy also works with cypress as a dev dependency.
    
    Changes:
    * Edited and rearranged server.js such that the app correctly shows the react application
    * Moved the cypress dependency from frontend/ to the root package.json. This change avoids the cannot find module common-tags error when deploying to Heroku
    
    Risk:
    * Cypress is still in the same directory as before (frontend/cypress), and hence all the cypress tests still go there. The script for running cypress (e.g. npm run cy:run) is to be run in the root folder, instead of in the frontend folder.
    * `frontend/eslintrc.json` does not have the cypress lint config, as cypress is no longer installed in `frontend` directory. This means that the cypress folder is not being checked for cypress rules i.e. `plugin:cypress/recommended`

--- src/server.js
+++ src/server.js
@@ -3,22 +3,34 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const connectToDatabase = require("./database/db-connect");
 
+const connectToDatabase = require("./database/db-connect");
 const indexRouter = require("./routes/index");
 
-const app = express();
 const port = process.env.PORT || "3001";
 
+const resolvedDirPath = path.resolve("./frontend/build");
+const resolvedPath = path.resolve("./frontend/build/index.html");
+
+const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, "frontend", "build")));
-
 // use API routers for different URLs
-app.use("/", indexRouter);
+app.use("/api", indexRouter);
+
+// This middleware informs the express application to serve our compiled React files
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static(resolvedDirPath));
+  app.get("*", (req, res) => {
+    res.sendFile(resolvedPath);
+  });
+}
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -34,10 +46,6 @@ app.use((err, req, res) => {
   // render the error page
   res.status(err.status || 500);
   res.send(`Error: ${err.status} - ${err.message}`);
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 // Connect to the database
